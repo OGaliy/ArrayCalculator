bf5704701e9c50f993d9ed927249a7e32c329751
Oleksandr <o.galiy27@gmail.com>
2021-08-17 21:07:12 +0300

    Added TriangularMatrixCalculator

--- MatrixCalculator/MatrixCalculator/Constants.cs
+++ MatrixCalculator/MatrixCalculator/Constants.cs
@@ -6,5 +6,6 @@
         public const string NotSquareException = "Not square matrix!";
         public const string WrongSizeException = "The matrix should have same size!";
         public const string ZeroDeterminantException = "Determinant equal 0!";
+        public const string RowOutOfTheRangeException = "Row is out of the range!";
     }
 }
---
+++ MatrixCalculator/MatrixCalculator/TriangularMatrixCalculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace MatrixCalculateLibrary
+{
+    public static class TriangularMatrixCalculator
+    {
+        /// <summary>
+        /// Converts matrix to triangular form.
+        /// </summary>
+        /// <param name="matrix">Input matrix</param>
+        /// <param name="roundIndex">Number for matrix elements round</param>
+        /// <returns>Triangular matrix</returns>
+        public static double[,] CalculateTriangularMatrix(double[,] matrix, int roundIndex)
+        {
+            int index = 0;
+            int rowLength = matrix.GetLength(0);
+            int columnLength = matrix.GetLength(1);
+
+            while (index < columnLength - 1)
+            {
+                if (index < columnLength - 2)
+                {
+                    SortByRowMaxValue(matrix, index);
+                }
+
+                for (int i = rowLength - 1; i >= index; i--)
+                {
+                    double multiplier = matrix[i + 1, index] / matrix[i, index];
+
+                    for (int j = index; j < columnLength; j++)
+                    {
+                        matrix[i + 1, j] = Math.Round(multiplier * matrix[i + 1, j], 2);
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Sorts matrix by row max value.
+        /// </summary>
+        /// <param name="matrix">Input matrix</param>
+        /// <param name="columnIndex">Column element which will compare</param>
+        /// <returns>Returns sorted matrix</returns>
+        public static double[,] SortByRowMaxValue(double[,] matrix, int columnIndex)
+        {
+            int length = matrix.GetLength(0);
+
+            for (int i = 1; i < length; i++)
+            {
+                for (int j = columnIndex; j < length - i; j++)
+                {
+                    if (matrix[j, columnIndex] < matrix[j + 1, columnIndex])
+                    {
+                        SwapLines(matrix, j, j + 1);
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Swaps lines in matrix.
+        /// </summary>
+        /// <param name="matrix">Input matrix</param>
+        /// <param name="first">Index of first row</param>
+        /// <param name="second">Index of second row</param>
+        /// <returns>Matrix with swaped lines</returns>
+        public static double[,] SwapLines(double[,] matrix, int first, int second)
+        {
+            int length = matrix.GetLength(1);
+
+            if (first > length - 1 || first < 0
+                || second > length - 1 || second < 0)
+            {
+                throw new MatrixExeption(Constants.RowOutOfTheRangeException);
+            }
+
+            double[] temp = new double[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                temp[i] = matrix[second, i];
+                matrix[second, i] = matrix[first, i];
+                matrix[first, i] = temp[i];
+            }
+
+            return matrix;
+        }
+    }
+}
